5b1fe796c2abc2885feffbf38ce64b07270f8b90
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-27 14:38:24 +0430

    setup the components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./Header";
+
 const App = () => {
-  
   return (
     <div className="App">
+      <Header />
       <h1>App</h1>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Foot React Menu.co</h1>
+    </div>
+  );
+};
+
+export default Header;
